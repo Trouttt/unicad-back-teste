00443fdd775f07f9a731ec7737c43a8747b173f6
leonardo <desu987@hotmail.com>
2023-02-13 20:19:32 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,6 @@ async function bootstrap() {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // another common pattern
-    //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader(
       'Access-Control-Allow-Methods',
       'GET,OPTIONS,PATCH,DELETE,POST,PUT',
