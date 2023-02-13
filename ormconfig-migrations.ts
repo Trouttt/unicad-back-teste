0ccb589a2ce0ada7c00442152a7aff71d341628a
leonardo <desu987@hotmail.com>
2023-02-13 13:14:49 -0300

    teste

--- ormconfig-migrations.ts
+++ ormconfig-migrations.ts
@@ -20,7 +20,7 @@ const dataSourceOptions: DataSourceOptions = {
   database: process.env.DB_DATABASE,
   ssl: true,
   migrations: [migrationsDir],
-  entities: ['/src/modules/**/entities/*.entity.ts'],
+  entities: ['dist/src/modules/**/entities/*.entity.js'],
   synchronize: false,
 };
 
