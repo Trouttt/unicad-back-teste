70d4e751c6735fa418cf395741fca01e5f2e5e2b
leonardo <desu987@hotmail.com>
2023-02-13 09:40:47 -0300

    Fix(config): added ssl

--- ormconfig-migrations.ts
+++ ormconfig-migrations.ts
@@ -18,6 +18,7 @@ const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: false,
   migrations: [migrationsDir],
   entities: ['dist/src/modules/**/*.entity.js'],
   synchronize: false,
