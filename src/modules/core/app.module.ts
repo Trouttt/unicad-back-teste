87da82013a34bbf5ed8e5a8339ff9b6d01786dbb
leonardo <desu987@hotmail.com>
2023-02-13 09:50:29 -0300

    Refactor(module): changed import

--- src/modules/core/app.module.ts
+++ src/modules/core/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfigService } from 'src/shared/typeorm/config/typeorm-config.service';
+import { TypeOrmConfigService } from '../../shared/typeorm/config/typeorm-config.service';
 import { DeliveriesModule } from '../deliveries/deliveries.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
