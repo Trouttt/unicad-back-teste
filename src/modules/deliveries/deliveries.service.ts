b097f748a1f2958b525e5c2112ae1d7fe1e4be7f
leonardo <desu987@hotmail.com>
2023-02-13 11:36:54 -0300

    teste

--- src/modules/deliveries/deliveries.service.ts
+++ src/modules/deliveries/deliveries.service.ts
@@ -30,7 +30,7 @@ export class DeliveriesService {
         take: pageOptionsDto.take,
         skip: pageOptionsDto.skip,
       });
-    console.log('testee');
+    const a = 10;
     const pageMetaDataDto = new PageMetaDataDto({
       item_count: itemCount,
       page_options_dto: pageOptionsDto,
