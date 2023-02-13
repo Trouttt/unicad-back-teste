fa8ccb6a99e08e64dbd1884b1fa26edb38e5ce16
leonardo <desu987@hotmail.com>
2023-02-13 11:08:11 -0300

    Config(main): changed header cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
   const config = new Swagger();
